f79306756c7cd455b4d66884a5f9cf3425b0d89f
Cesar Abreu <cesarabreu_@outlook.com>
2023-10-22 20:47:06 -0300

    Adicionando a feature-14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -29,7 +29,7 @@ import { EditarReceitaComponent } from './editar-receita/editar-receita.componen
     ParentComponent,
     EditarReceitaComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
