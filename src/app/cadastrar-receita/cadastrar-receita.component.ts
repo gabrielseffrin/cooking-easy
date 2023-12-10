2fd6cea981412c15fe50c3237f4ff180a1833aab
gabrielseffrin <gabrielfseffrin@gmail.com>
2023-12-10 16:19:32 -0300

    .

--- src/app/cadastrar-receita/cadastrar-receita.component.ts
+++ src/app/cadastrar-receita/cadastrar-receita.component.ts
@@ -28,14 +28,15 @@ export class CadastrarReceitaComponent {
     if (this.cadastrarReceita.valid) {
       const formData = this.cadastrarReceita.value;
       console.log(this.cadastrarReceita.value);
+
       localStorage.setItem('userData', JSON.stringify(formData));
 
       const postData = {
         nome: formData.name,
         ingredientes: formData.ingredientes,
         foto: formData.foto,
-        estado_receita: 1,
-        privacidade_receita: 1,
+        estado_receita: 2,
+        privacidade_receita: formData.privacidade,
       };
 
       this.http.post('http://localhost:3000/receitas', postData).subscribe(
